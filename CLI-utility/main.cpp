81632f0b73fad7dd4110e3424860a0ddbe91a6a5
Rx1513 <peaceful0strangeac@gmail.com>
2024-05-23 21:51:42 +0300

    Add functionality to get package list for given branch
    Include boost library and implement reading package list from
    rdb.altlinux.org/api

--- CLI-utility/main.cpp
+++ CLI-utility/main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 #include <string>
 
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/beast.hpp>
+#include <boost/lexical_cast.hpp>
+
+using namespace boost::beast;
+using namespace boost::asio;
+
+template<
+    class SyncReadStream,
+    class DynamicBuffer,
+    bool isRequest, class Body, class Allocator>
+void
+read_large_response_body(
+    SyncReadStream& stream,
+    DynamicBuffer& buffer,
+    http::message<isRequest, Body, http::basic_fields<Allocator>>& message,
+    std::size_t bodyLimit)
+{
+    http::parser<isRequest, Body, Allocator> parser(std::move(message));
+    
+    parser.eager(true);
+    parser.body_limit(bodyLimit);
+    
+    http::read(stream, buffer, parser);
+    
+    message = parser.release();
+}
+
+std::string
+request_packages_from_branch(std::string branch) {
+    const std::string host = "rdb.altlinux.org";
+    const std::string path = "/api/export/branch_binary_packages/" + branch;
+    const std::string port = "443";
+
+    io_service service;
+
+    ssl::context context(ssl::context::sslv23_client);
+    ssl::stream<ip::tcp::socket> ssocket = { service, context };
+    ip::tcp::resolver resolver(service);
+    auto it = resolver.resolve(host, port);
+    connect(ssocket.lowest_layer(), it);
+    ssocket.handshake(ssl::stream_base::handshake_type::client);
+    http::request<http::string_body> request{ http::verb::get, path, 11 };
+    request.set(http::field::host, host);
+    http::write(ssocket, request);
+    http::response<http::string_body> response;
+    flat_buffer buffer;
+
+    read_large_response_body(ssocket, buffer, response,80*1024*1024);
+    std::string response_body = boost::lexical_cast<std::string>(response.body());
+
+    return response_body;
+}
+
 int main( int argc, char* argv[] ) {
     while (true) {
         std::string firstBranch,
@@ -11,10 +66,12 @@ int main( int argc, char* argv[] ) {
         std::cout << "Enter second branch: ";
         std::cin >> secondBranch;
 
+        // request_packages_from_branch(firstBranch);
+        // request_packages_from_branch(secondBranch);
+
         /*
         TODO:
 
-        string? some_local_function_that_takes_user_input_and_returns_JSON_response/s_from_ALTREPO_server(string first_branch, string second_branch)
         
         Some confdition that checks succes of requests
         {

a3f1bdd9bbecc63f6fa55ca718cfaabf88fe6900
Rx1513 <peaceful0strangeac@gmail.com>
2024-05-24 23:04:39 +0300

    Implement package presence check
    Take two branches and check if package belongs only
    to a first branch or a second branch.

--- Package compare lib/lib.cpp
+++ Package compare lib/lib.cpp
@@ -4,123 +4,54 @@
 
 using namespace boost::json;
 
-
-
-// {
-//   "request_args": {},
-//   "length": 0,
-//   "packages": [
-//     {
-//       "name": "string",
-//       "epoch": 0,
-//       "version": "string",
-//       "release": "string",
-//       "arch": "string",
-//       "disttag": "string",
-//       "buildtime": 0,
-//       "source": "string"
-//     }
-//   ]
-// }
-
-// {
-//   "first_branch: string",
-//   "second_branch: string",
-// [
-//   "arch: string",
-//   "packages_presented_only_in_first_bracnh": {
-//      "length": 0,
-//      "list": [
-//          {
-//          "name": "string",
-//          "epoch": 0,
-//          "version": "string",
-//          "release": "string",
-//          "arch": "string",
-//          "disttag": "string",
-//          "buildtime": 0,
-//          "source": "string"
-//          }
-//      ]
-//  },
-//   "packages_presented_only_in_second_bracnh": {
-//      "length": 0,
-//      "list": [
-//          {
-//          "name": "string",
-//          "epoch": 0,
-//          "version": "string",
-//          "release": "string",
-//          "arch": "string",
-//          "disttag": "string",
-//          "buildtime": 0,
-//          "source": "string"
-//          }
-//      ]
-//  },
-//   "packages_with_version_older_in_first_branch": {
-//      "length": 0,
-//      "list": [
-//          {
-//          "name": "string",
-//          "epoch": 0,
-//          "version": "string",
-//          "release": "string",
-//          "arch": "string",
-//          "disttag": "string",
-//          "buildtime": 0,
-//          "source": "string"
-//          }
-//      ]
-//  }
-// }
-// ]
-// }
-
-// void
-// add_package_to_result_json(object& result_json, object package){
-//     array package_array = result_json.at("package_list").as_array();
-//     for (auto package_list : package_array)
-//     {
-//         auto x = package_list.as_object();
-//         if(x.at("arch") == package.at("arch")){
-//             x.at("packages_presented_only_in_first_bracnh").as_object().at("list").as_array().push_back(package);
-//             return;
-//         }
-//     }
-//     package_array.push_back(
-//         {
-//             {"arch", package.at("arch") },
-//             {"packages_presented_only_in_first_bracnh", { "list", {} }}
-//         });
-// }
+void push_package_to_given_list(std::string list_name, object& result_json, object package){
+    array& package_array = result_json.at("package_list").as_array();
+    for (value& package_list_json_object : package_array)
+    {
+        // std::cout << package_list_json_object << std::endl << std::endl;;
+        object package_list = package_list_json_object.as_object();
+        if(package_list.at("arch") == package.at("arch")){
+            package_list.at(list_name).as_array().push_back(package);
+            return;
+        }
+    }
+    package_array.push_back(
+        {
+            { "arch", package.at("arch") },
+            { list_name, array{ package } }
+        });
+}
 
 std::string compare_branches(
     std::string first_branch_packages, 
-    std::string second_branch_packages
+    std::string second_branch_packages,
+    std::string first_branch,
+    std::string second_branch
     )
 {
     array first_branch_json_packages = parse(first_branch_packages).as_object().at("packages").as_array();
     array second_branch_json_packages = parse(second_branch_packages).as_object().at("packages").as_array();
     
-    object result_json = { { "package_list", array() } };
-
+    object result_json = { 
+        { "package_list", array() },
+        { "first_branch", first_branch },
+        { "second_branch", second_branch}
+    };
     std::size_t last_second_index = 0; 
+    
     for (std::size_t first_package_index = 0; first_package_index < first_branch_json_packages.size(); ++first_package_index)
     {
-        object first_package = first_branch_json_packages[first_package_index].as_object();
+        object& first_package = first_branch_json_packages[first_package_index].as_object();
         bool first_package_not_found = true;
         for (std::size_t second_package_index = last_second_index; second_package_index < second_branch_json_packages.size(); ++second_package_index)
         {
-
-            object second_package = second_branch_json_packages[second_package_index].as_object();
+            object& second_package = second_branch_json_packages[second_package_index].as_object();
 
             /*
-                Since all packages sorted in ascending order of arch names and then for package name
-                we can skip further checks
+                TODO:
+                Modify condition for names
             */
-
-            if( first_package.at("name").as_string()[0] > second_package.at("name").as_string()[0] ) { break; }
+            if ( first_package.at("arch").as_string()[0] < second_package.at("arch").as_string()[0] ) { break; }
 
             /*
                 if names of packages are equal, then push all second branch packages 
@@ -129,21 +60,22 @@ std::string compare_branches(
                 and at the end compare package versions
             */ 
 
-            if( first_package.at("name") == second_package.at("name"))
+            if( first_package.at("name") == second_package.at("name") && first_package.at("arch") == second_package.at("arch"))
             {
                 first_package_not_found = false;
                 for (std::size_t i = last_second_index; i < second_package_index; ++i)
                 {
-                    // push to packages presented in second branch
+                    push_package_to_given_list("packages_presented_only_in_second_branch",result_json,second_package);
                 }
                 last_second_index = second_package_index + 1;
                 // compare versions
                 break;
             }
         }
+
         if (first_package_not_found)
         {
-             // push to packages presented in first branch
+            push_package_to_given_list("packages_presented_only_in_first_branch",result_json,first_package);
         }
     }
 
--- Package compare lib/lib.hpp
+++ Package compare lib/lib.hpp
@@ -1,3 +1,3 @@
 #include <string>
 
-std::string compare_branches(std::string first_branch_packages, std::string second_branch_packages);
+std::string compare_branches(std::string first_branch_packages, std::string second_branch_packages, std::string first_branch, std::string second_branch);

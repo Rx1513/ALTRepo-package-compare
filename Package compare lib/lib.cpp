291c21f40ec60433886ce09a2f3a2d9113902793
Rx1513 <peaceful0strangeac@gmail.com>
2024-05-25 09:07:01 +0300

    Implement package version comparison
    Implement calling command rpmvercmp to check the difference in
    versions of packages presented into branches.

--- Package compare lib/lib.cpp
+++ Package compare lib/lib.cpp
@@ -1,6 +1,25 @@
 #include "lib.hpp"
 #include <iostream>
 #include <boost/json/src.hpp>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
 
 using namespace boost::json;
 
@@ -8,7 +27,6 @@ void push_package_to_given_list(std::string list_name, object& result_json, obje
     array& package_array = result_json.at("package_list").as_array();
     for (value& package_list_json_object : package_array)
     {
-        // std::cout << package_list_json_object << std::endl << std::endl;;
         object package_list = package_list_json_object.as_object();
         if(package_list.at("arch") == package.at("arch")){
             package_list.at(list_name).as_array().push_back(package);
@@ -20,6 +38,15 @@ void push_package_to_given_list(std::string list_name, object& result_json, obje
             { "arch", package.at("arch") },
             { list_name, array{ package } }
         });
+}
+
+bool compare_version(const object& first_package,const object& second_package)
+{
+    std::string commad = std::string("rpmvercmp ") + 
+    std::to_string(first_package.at("epoch").as_int64()) + ':'  + value_to<std::string>(first_package.at("version")) + '-'  + value_to<std::string>(first_package.at("release"))
+    + ' ' + 
+    std::to_string(second_package.at("epoch").as_int64()) + ':'  + value_to<std::string>(second_package.at("version")) + '-'  + value_to<std::string>(second_package.at("release"));
+    return std::stoi(exec(commad.c_str())) > 0;
 }
 
 std::string compare_branches(
@@ -68,6 +95,9 @@ std::string compare_branches(
                     push_package_to_given_list("packages_presented_only_in_second_branch",result_json,second_package);
                 }
                 last_second_index = second_package_index + 1;
+                if(compare_version(first_package,second_package)) {
+                    push_package_to_given_list("packages_with_bigger_version_in_first_branch",result_json,second_package);
+                };
                 // compare versions
                 break;
             }
